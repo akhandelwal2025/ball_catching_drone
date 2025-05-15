b714f795ade7f678951f78fcc2dca7e181c6fa11
Ankit Khandelwal <akhandelwal2025@gmail.com>
2025-05-15 15:37:32 -0400

    starting to get the viz to work

--- scripts/test_lookat.py
+++ scripts/test_lookat.py
@@ -13,7 +13,6 @@ def draw_axes(ax,
         rotation = np.apply_along_axis(func1d=utils.generate_rotation_matrix_from_eulers, 
                                         axis=1,
                                         arr=eulers)
-        print(f"computed rotation: {rotation}")
     for i in range(N):
         ax.quiver(origin[i, 0], origin[i, 1], origin[i, 2], *rotation[i, :, 0], color='red', pivot='tail', length=2, normalize=True)
         ax.quiver(origin[i, 0], origin[i, 1], origin[i, 2], *rotation[i, :, 1], color='green', pivot='tail', length=2, normalize=True)
@@ -24,18 +23,12 @@ target = np.array([1., -1., -1.])[np.newaxis, :]
 up = np.array([0., 0., 1.])[np.newaxis, :]
 R = utils.lookat(origin, target, up, return_eulers=False)
 eulers = utils.lookat(origin, target, up, return_eulers=True)
-print(f"rotation from lookat: {R}, determinant: {np.linalg.det(R)}")
-print(f"eulers from lookat: {eulers}")
-# print(eulers)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.set_xlim(-2, 10)  # Set x-axis limits to a range around zero
 ax.set_ylim(-2, 10)  # Set y-axis limits to a range around zero
 ax.set_zlim(0, 12)
-
-# draw_axes(ax, origin, rotation=np.eye(3)[np.newaxis, :, :])
-# draw_axes(ax, origin, rotation=R)
 
 draw_axes(ax, origin, eulers=np.array([0., 0., 0.,])[np.newaxis, :])
 draw_axes(ax, origin, eulers=eulers)
--- src/env.py
+++ src/env.py
@@ -6,33 +6,36 @@ class TestEnv():
     def __init__(self, cfg):
         self.cfg = cfg
 
-        # start with pos specified by cfg. target cam pos is something slightly off axis from that
+        # start at pos, orientation specified by cfg
         self.cam_pos = np.stack([self.cfg['pos']['cam1'],
                                  self.cfg['pos']['cam2'],
                                  self.cfg['pos']['cam3'],
                                  self.cfg['pos']['cam4']], axis=0)
-        self.gt_cam_pos = np.random.normal(loc=self.cam_pos, scale=self.cfg['pos']['std'])
+        self.cam_eulers = np.stack([self.cfg['eulers']['cam1'],
+                                    self.cfg['eulers']['cam2'],
+                                    self.cfg['eulers']['cam3'],
+                                    self.cfg['eulers']['cam4']], axis=0)
+        self.cam_eulers = self.cam_eulers * (np.pi / 180.) # deg to rad conversion
 
+        # gt cam pos, orientation are generated
+        # if 'use_lookat' is True, then cam orientation is set to point at a target location
+        # otherwise, the orientation is randomized according to the std
+        self.gt_cam_pos = np.random.normal(loc=self.cam_pos, scale=self.cfg['pos']['std'])
         if self.cfg['use_lookat']:
+            self.gt_cam_eulers = utils.lookat(origin=self.gt_cam_pos,
+                                              target=np.array(self.cfg['lookat'])[np.newaxis, :], # newaxis enables broadcasting
+                                              up=np.array([0., 0., 1.])[np.newaxis, :],
+                                              return_eulers=True) 
         else:
-            self.cam_eulers = np.stack([self.cfg['eulers']['cam1'],
-                                        self.cfg['eulers']['cam2'],
-                                        self.cfg['eulers']['cam3'],
-                                        self.cfg['eulers']['cam4']], axis=0)
-            self.cam_eulers = self.cam_eulers * (np.pi / 180.) # deg to rad conversion
-            
-        self.randomize_cams()
-        self.gt_cam_eulers = self.gt_cam_eulers * (np.pi / 180.)
+            self.gt_cam_eulers = self.gt_cam_eulers * (np.pi / 180.)            
 
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111, projection='3d')
         self.ax.set_xlim(-2, 10)  # Set x-axis limits to a range around zero
         self.ax.set_ylim(-2, 10)  # Set y-axis limits to a range around zero
         self.ax.set_zlim(0, 12)
-
-    def randomize_cams(self):
-        self.gt_cam_pos = np.random.normal(loc=self.cam_pos, scale=self.cfg['pos']['std'])
-        self.gt_cam_eulers = np.random.normal(loc=self.cam_eulers, scale=self.cfg['eulers']['std'])
+        if self.cfg['use_lookat']:
+            self.ax.scatter(self.cfg['lookat'][0], self.cfg['lookat'][1], self.cfg['lookat'][2], 'yellow')
     
     def draw_axes(self,
                   origin: np.ndarray,
@@ -44,17 +47,17 @@ class TestEnv():
                                        axis=1,
                                        arr=eulers)
         for i in range(N):
-            self.ax.quiver(origin[i, 0], origin[i, 1], origin[i, 2], *rotation[i, :, 0], color=axis_color, pivot='tail', length=2, normalize=True)
-            self.ax.quiver(origin[i, 0], origin[i, 1], origin[i, 2], *rotation[i, :, 1], color=axis_color, pivot='tail', length=2, normalize=True)
-            self.ax.quiver(origin[i, 0], origin[i, 1], origin[i, 2], *rotation[i, :, 2], color=axis_color, pivot='tail', length=2, normalize=True)
+            self.ax.quiver(origin[i, 0], origin[i, 1], origin[i, 2], *rotation[i, :, 0], color='red', pivot='tail', length=2, normalize=True)
+            self.ax.quiver(origin[i, 0], origin[i, 1], origin[i, 2], *rotation[i, :, 1], color='green', pivot='tail', length=2, normalize=True)
+            self.ax.quiver(origin[i, 0], origin[i, 1], origin[i, 2], *rotation[i, :, 2], color='blue', pivot='tail', length=2, normalize=True)
 
     def render(self):
         # render gt_cams
-        self.ax.scatter(self.gt_cam_pos[:, 0], self.gt_cam_pos[:, 1], self.gt_cam_pos[:, 2], color='green')
+        self.ax.scatter(self.gt_cam_pos[:, 0], self.gt_cam_pos[:, 1], self.gt_cam_pos[:, 2], color='green', s=30)
         self.draw_axes(self.gt_cam_pos, self.gt_cam_eulers, 'green')
 
         # render current cams
-        self.ax.scatter(self.cam_pos[:, 0], self.cam_pos[:, 1], self.cam_pos[:, 2], color='red')
+        self.ax.scatter(self.cam_pos[:, 0], self.cam_pos[:, 1], self.cam_pos[:, 2], color='red', s=30)
         self.draw_axes(self.cam_pos, self.cam_eulers, 'red')
         plt.show()
 
