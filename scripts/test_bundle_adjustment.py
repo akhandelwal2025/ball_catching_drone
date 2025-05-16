3f97f19557b3847da895296fbdba430acacf563d
Ankit Khandelwal <akhandelwal2025@gmail.com>
2025-05-16 11:21:23 -0400

    FOV checking + random pt gen working

--- scripts/test_bundle_adjustment.py
+++ scripts/test_bundle_adjustment.py
@@ -6,7 +6,10 @@ def main(args):
     with open(args.env_cfg, "r") as file:
         cfg = yaml.safe_load(file)
     env = TestEnv(cfg)
-    env.render()
+    while True:
+        feature_pt = env.gen_random_pt()
+        print(env.pt_in_fovs(feature_pt))
+        env.render()
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Description of your program')
     parser.add_argument('--env_cfg', type=str, default='cfgs/test_env.yaml')
--- src/env.py
+++ src/env.py
@@ -15,7 +15,7 @@ class TestEnv():
                                     self.cfg['eulers']['cam2'],
                                     self.cfg['eulers']['cam3'],
                                     self.cfg['eulers']['cam4']], axis=0)
-        self.cam_eulers = self.cam_eulers * (np.pi / 180.) # deg to rad conversion
+        self.cam_eulers = np.radians(self.cam_eulers)
 
         # gt cam pos, orientation are generated
         # if 'use_lookat' is True, then cam orientation is set to point at a target location
@@ -27,16 +27,15 @@ class TestEnv():
                                               up=np.array([0., 0., 1.])[np.newaxis, :],
                                               return_eulers=True) 
         else:
-            self.gt_cam_eulers = self.gt_cam_eulers * (np.pi / 180.)            
+            self.gt_cam_eulers = np.radians(self.gt_cam_eulers)            
 
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111, projection='3d')
         self.ax.set_xlim(-2, 10)  # Set x-axis limits to a range around zero
         self.ax.set_ylim(-2, 10)  # Set y-axis limits to a range around zero
         self.ax.set_zlim(0, 12)
-        if self.cfg['use_lookat']:
-            self.ax.scatter(self.cfg['lookat'][0], self.cfg['lookat'][1], self.cfg['lookat'][2], 'yellow')
-    
+        plt.ion()
+
     def draw_axes(self,
                   origin: np.ndarray,
                   eulers: np.ndarray,
@@ -54,9 +53,9 @@ class TestEnv():
     def draw_cones(self,
                    origins: np.ndarray,
                    eulers: np.ndarray):
-        theta = np.radians(30)      # Half angle of FOV
-        L = 5.0                     # Length of the cone
-        r = L * np.tan(theta)       # Compute base radius from FOV and length
+        theta = np.radians(self.cfg['cam_FOV_half_angle'])
+        L = self.cfg['cone_length']
+        r = L * np.tan(theta) # Compute base radius from FOV and length
 
         # generate cone aligned with +z
         u = np.linspace(0, 2*np.pi, 100)
@@ -88,7 +87,35 @@ class TestEnv():
                                  color='black', 
                                  alpha=0.1)
 
+    def gen_random_pt(self):
+        bounds = np.full((3,), self.cfg['point_gen']['radius'])
+        delta = np.random.uniform(low=-bounds,
+                                  high=bounds)
+        self.feature_pt = np.array(self.cfg['point_gen']['center']) + delta
+        return self.feature_pt
+    
+    def pt_in_fovs(self,
+                   pt):
+        """
+        Given a pt, check if it is in the FOV of all cameras
+        Inputs:
+            pt: np.ndarray - shape = (3,)
+        Output True if pt can be seen by all cameras, otherwise False
+        """
+        for i in range(len(self.gt_cam_pos)):
+            origin = self.gt_cam_pos[i]
+            R = utils.generate_rotation_matrix_from_eulers(self.gt_cam_eulers[i])
+            forward_dir_wf = R @ np.array([1., 0., 0.])
+            forward_dir_wf = forward_dir_wf / np.linalg.norm(forward_dir_wf)
+            unit_dir = (pt-origin)/np.linalg.norm(pt-origin)
+            theta = np.arccos(np.dot(unit_dir, forward_dir_wf))
+            if theta > np.radians(self.cfg['cam_FOV_half_angle']):
+                return False
+        return True
+
     def render(self):
+        self.ax.cla()
+
         # render gt_cams
         self.ax.scatter(self.gt_cam_pos[:, 0], self.gt_cam_pos[:, 1], self.gt_cam_pos[:, 2], color='green', s=30)
         self.draw_axes(self.gt_cam_pos, self.gt_cam_eulers, 'green')
@@ -97,7 +124,17 @@ class TestEnv():
         self.ax.scatter(self.cam_pos[:, 0], self.cam_pos[:, 1], self.cam_pos[:, 2], color='red', s=30)
         self.draw_axes(self.cam_pos, self.cam_eulers, 'red')
 
-        self.draw_cones(self.gt_cam_pos, self.gt_cam_eulers)
-        plt.show()
+        # render FOV cones
+        if self.cfg['draw_FOV']:
+            self.draw_cones(self.gt_cam_pos, self.gt_cam_eulers)
+        
+        # render lookat point
+        if self.cfg['use_lookat']:
+            self.ax.scatter(self.cfg['lookat'][0], self.cfg['lookat'][1], self.cfg['lookat'][2])
+        
+        # render feature point
+        self.ax.scatter(self.feature_pt[0], self.feature_pt[1], self.feature_pt[2], color='red', s=30)
 
+        plt.show()
+        plt.pause(0.01)
         
