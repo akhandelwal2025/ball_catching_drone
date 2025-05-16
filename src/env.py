3a7d9a1328f91f843ca9a5addbc289245dc17b41
Ankit Khandelwal <akhandelwal2025@gmail.com>
2025-05-16 10:21:30 -0400

    cam FOV cones are now being drawn

--- src/env.py
+++ src/env.py
@@ -51,6 +51,43 @@ class TestEnv():
             self.ax.quiver(origin[i, 0], origin[i, 1], origin[i, 2], *rotation[i, :, 1], color='green', pivot='tail', length=2, normalize=True)
             self.ax.quiver(origin[i, 0], origin[i, 1], origin[i, 2], *rotation[i, :, 2], color='blue', pivot='tail', length=2, normalize=True)
 
+    def draw_cones(self,
+                   origins: np.ndarray,
+                   eulers: np.ndarray):
+        theta = np.radians(30)      # Half angle of FOV
+        L = 5.0                     # Length of the cone
+        r = L * np.tan(theta)       # Compute base radius from FOV and length
+
+        # generate cone aligned with +z
+        u = np.linspace(0, 2*np.pi, 100)
+        v = np.linspace(0, 1, 80)
+        u, v = np.meshgrid(u, v)
+        x = v*r*np.cos(u)
+        y = v*r*np.sin(u)
+        z = v*L
+        pts = np.stack([x.ravel(), y.ravel(), z.ravel()], axis=0)
+
+        # cone by default is oriented around +z. want to rotate so that cone is oriented along +x instead
+        orient_along_x = np.array([
+            [0., 0., 1.],
+            [0., 1., 0.],
+            [-1., 0., 0.]
+        ])
+        pts = orient_along_x @ pts
+        pts = np.vstack((pts, np.ones((1, pts.shape[1])))) # add homogenous coordinates
+
+        for i in range(len(origins)):
+            origin = origins[i][:, np.newaxis]
+            rot_mtrx = utils.generate_rotation_matrix_from_eulers(eulers[i])
+            P = np.vstack((np.hstack((rot_mtrx, origin)), 
+                           np.array([[0., 0., 0., 1.]])))
+            transformed_pts = P @ pts
+            self.ax.plot_surface(transformed_pts[0, :].reshape(x.shape), 
+                                 transformed_pts[1, :].reshape(y.shape), 
+                                 transformed_pts[2, :].reshape(z.shape), 
+                                 color='black', 
+                                 alpha=0.1)
+
     def render(self):
         # render gt_cams
         self.ax.scatter(self.gt_cam_pos[:, 0], self.gt_cam_pos[:, 1], self.gt_cam_pos[:, 2], color='green', s=30)
@@ -59,6 +96,8 @@ class TestEnv():
         # render current cams
         self.ax.scatter(self.cam_pos[:, 0], self.cam_pos[:, 1], self.cam_pos[:, 2], color='red', s=30)
         self.draw_axes(self.cam_pos, self.cam_eulers, 'red')
+
+        self.draw_cones(self.gt_cam_pos, self.gt_cam_eulers)
         plt.show()
 
         
