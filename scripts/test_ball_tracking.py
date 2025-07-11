lot of changes, debugging why the position tracking is not giving accurate numbers. I think something was def wrong with rotation matrix creation

scripts/test_ball_tracking.py
@@ -1,33 +1,48 @@
 from pseyepy import Camera
-from src.mocap import Mocap
+from src.mocap import PsEyeMocap
 import time
 import cv2
 import numpy as np
 import argparse
 import yaml
+import src.utils as utils
 
 # BLACK HSV BOUNDS
-LOWER = np.array([0., 0., 0.])
-UPPER = np.array([50., 50., 50.])
+LOWER = np.array([50, 50, 50], dtype=np.uint8)
+UPPER = np.array([255, 255, 255], dtype=np.uint8)
 
 def main(args):
     with open(args.mocap_cfg, "r") as file:
         cfg = yaml.safe_load(file)
-    mocap = Mocap(cfg)
+    mocap = PsEyeMocap(cfg)
+    breakpoint()
     start = time.time()
     frames = 0
     track_ball_time = 0
     try:
         while True:
             iter_start = time.time()
-            img, center = mocap.track_ball(lower=LOWER,
-                                              upper=UPPER)
+            # img = mocap.read_cameras()[np.newaxis, :, :]
+            imgs = mocap.read_cameras()
+            imgs = imgs.copy()
+            centers = mocap.locate_centers(imgs=imgs,
+                                          num_centers=1,
+                                          lower=LOWER,
+                                          upper=UPPER)
+            centers = centers.reshape((centers.shape[0] * centers.shape[1], centers.shape[2]))
+            print(centers, centers.shape)
             iter_end = time.time()
             track_ball_time += iter_end - iter_start
-
-            dot = cv2.circle(img, center, radius=3, color=[0, 0, 255])
-            cv2.imshow("ball tracking", dot)
-            cv2.waitKey(1)
+            pt_3d = utils.DLT(centers, mocap.projections_wf)
+            print(pt_3d)
+            mocap.render()
+            # frames += 1
+            for i in range(len(imgs)):
+                img = imgs[i]
+                center = centers[i]
+                dot = cv2.circle(img, (int(center[0]), int(center[1])), radius=3, color=[0, 0, 255])
+                cv2.imshow(f"Cam {i+1}", dot)
+                cv2.waitKey(1)
             frames += 1          
     except BaseException as e:
         print(e)
@@ -38,7 +53,8 @@ def main(args):
 
 
 if __name__ == "__main__":
+    np.set_printoptions(precision=3, suppress=True)
     parser = argparse.ArgumentParser(description='Description of your program')
-    parser.add_argument('--mocap_cfg', type=str, default='cfgs/mocap.yaml')
+    parser.add_argument('--mocap_cfg', type=str, default='cfgs/PSEyeMocap.yaml')
     args = parser.parse_args()  
     main(args)

src/mocap.py
@@ -27,7 +27,9 @@ class FakeCameras():
 class BaseMocap(ABC):
     def __init__(self, mocap_cfg):
         self.mocap_cfg = mocap_cfg
-        self.vis = Vis(self.mocap_cfg)
+        self.vis = Vis(self.mocap_cfg,
+                       self.cam_pos,
+                       self.cam_eulers)
 
         self.construct_intrinsics()
         self.construct_extrinsics_wf()
@@ -99,24 +101,28 @@ class BaseMocap(ABC):
             upper: List[int] - upper bound of values to use to create mask
         """
         num_imgs = imgs.shape[0]
-        centers = np.full((num_imgs, num_centers, 2), -1)
+        centers = np.zeros((num_imgs, num_centers, 2), dtype=np.float32)
         for i in range(num_imgs):
             img = imgs[i]
+            # print("img.shape:", img.shape, "img.dtype:", img.dtype)
+            # print("lower:", lower, "shape:", np.array(lower).shape, "dtype:", np.array(lower).dtype)
+            # print("upper:", upper, "shape:", np.array(upper).shape, "dtype:", np.array(upper).dtype)\
             mask = cv2.inRange(img, lower, upper)
             if not self.mocap_cfg['use_fake_imgs']:
-                mask = cv2.erode(mask, None, iterations=2)
+                # mask = cv2.erode(mask, None, iterations=2)
                 mask = cv2.dilate(mask, None, iterations=2)
             contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
             contours = list(contours)
-            if len(contours) < num_centers:
-                return None
             contours.sort(key=cv2.contourArea, reverse=True) # assumption is that dots are the biggest thing in the img
-            contours = contours[:num_centers]
+            contours = contours[:min(len(contours), num_centers)]
             img_centers = []
-            for c in contours:
-                M = cv2.moments(c)
-                img_centers.append([int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])])
-            centers[i] = img_centers
+            for c in range(num_centers):
+                if c < len(contours):
+                    M = cv2.moments(contours[c])
+                    if M['m00'] != 0:
+                        img_centers.append([(M["m10"] / M["m00"]), (M["m01"] / M["m00"])])
+            if len(img_centers) != 0:
+                centers[i, :len(img_centers), :] = img_centers
         return centers
     
     def bundle_adjustment(self, n_obs):
@@ -249,19 +255,26 @@ class FakeMocap(BaseMocap):
 
 class PsEyeMocap(BaseMocap):
     def __init__(self, mocap_cfg):
-        self.c = Camera(mocap_cfg['pseyepy_params'])
         self.n_cams = mocap_cfg['n_cams']
-        self.resolution = (320, 240) if mocap_cfg['pseyepy_params']['resolution'] == 'small' else (640, 480)
+        if mocap_cfg['pseyepy_params']['resolution'] == 'small':
+            self.resolution = (320, 240)
+            mocap_cfg['pseyepy_params']['resolution'] = Camera.RES_SMALL
+        else:
+            self.resolution = (640, 480)
+            mocap_cfg['pseyepy_params']['resolution'] = Camera.RES_LARGE
+        # self.c = Camera(**mocap_cfg['pseyepy_params'])
 
-        self.cam_pos = np.stack([self.mocap_cfg['pos']['cam1'],
-                                 self.mocap_cfg['pos']['cam2'],
-                                 self.mocap_cfg['pos']['cam3'],
-                                 self.mocap_cfg['pos']['cam4']], axis=0)
-        self.cam_eulers = np.stack([self.mocap_cfg['eulers']['cam1'],
-                                    self.mocap_cfg['eulers']['cam2'],
-                                    self.mocap_cfg['eulers']['cam3'],
-                                    self.mocap_cfg['eulers']['cam4']], axis=0)
+        self.cam_pos = np.stack([mocap_cfg['pos']['cam1'],
+                                 mocap_cfg['pos']['cam2'],
+                                 mocap_cfg['pos']['cam3'],
+                                 mocap_cfg['pos']['cam4']], axis=0)
+        self.cam_eulers = np.stack([mocap_cfg['eulers']['cam1'],
+                                    mocap_cfg['eulers']['cam2'],
+                                    mocap_cfg['eulers']['cam3'],
+                                    mocap_cfg['eulers']['cam4']], axis=0)
+        self.cam_eulers = np.radians(self.cam_eulers)
         super().__init__(mocap_cfg)
+        breakpoint()
 
     def construct_intrinsics(self):
         self.intrinsics = np.empty((4, 3, 3))
@@ -289,13 +302,11 @@ class PsEyeMocap(BaseMocap):
         self.projections_c1f = self.intrinsics @ self.extrinsics_c1c
     
     def read_cameras(self):
-        self.imgs, timesteps = self.c.read()
+        imgs, timesteps = self.c.read()
+        self.imgs = np.array(imgs)
         return self.imgs
     
     def render(self,
-               feature_pts):
-        self.vis.render(imgs=self.imgs,
-                        frames3d={
-                            'red': (self.cam_pos, self.cam_eulers, True)
-                        },
-                        feature_pts=feature_pts)
+               centers = None,
+               feature_pts = None):
+        self.vis.render()

src/utils.py
@@ -19,7 +19,7 @@ def extract_eulers_from_rotation_matrix_XYZ(rot_mtrx):
     # breakpoint()
     # assert np.allclose(R.from_matrix(rot_mtrx).as_euler('xyz'), eulers)
     # return eulers
-    return R.from_matrix(rot_mtrx).as_euler('xyz')
+    return R.from_matrix(rot_mtrx).as_euler('XYZ')
 
 def lookat(origin, target, up, return_eulers=True):
     """
@@ -52,11 +52,11 @@ def lookat(origin, target, up, return_eulers=True):
     
 def generate_rotation_matrix_from_eulers(eulers):
     """
-    Given euler angles, generate rotation matrix using XYZ rotation
+    Given euler angles, generate rotation matrix using ZYX intrinsic rotation
     Inputs:
         eulers: list of euler angles
     Output:
-        np.ndarray, shape=(3,3) representing XYZ rotation
+        np.ndarray, shape=(3,3) representing ZYX rotation
     """
     x, y, z = eulers[0], eulers[1], eulers[2]
     calc = np.array([
@@ -72,7 +72,7 @@ def generate_rotation_matrix_from_eulers(eulers):
         [0., c(x), -s(x)],
         [0., s(x), c(x)]
     ])
-    assert np.allclose(calc, R.from_euler('xyz', eulers).as_matrix())
+    assert np.allclose(calc, R.from_euler('ZYX', eulers).as_matrix()), f"\n{calc}\n{R.from_euler('ZYX', eulers).as_matrix()}"
     return calc
 
 def eight_point_algorithm(left_frame_pts,
@@ -112,6 +112,7 @@ def DLT(pixels,
         3-element np.ndarray representing best guess for triangulated 3D point  
     """
     # vectorized creation of A - kinda overkill for four cams, but good numpy practice lol
+    print(pixels.shape)
     N = pixels.shape[0]
     row1 = pixels[:, 1][:, np.newaxis] * projections[:, 2, :] - projections[:, 1, :] # vP_2 - P_1
     row2 = projections[:, 0, :] - pixels[:, 0][:, np.newaxis] * projections[:, 2, :] # P_0 - uP_2
@@ -217,10 +218,15 @@ def construct_extrinsics(pos,
     R_wc = R_cw.T # world -> body
     # need to transform to +x left, +y up, +z forward
     # this ensures optical axis is aligned with +z enabling proper homogenous calculation
+    # R_wc = np.array([
+    #     [0., 1., 0.,], 
+    #     [0., 0., 1.], 
+    #     [1., 0., 0.,]
+    # ]) @ R_wc
     R_wc = np.array([
-        [0., 1., 0.,], 
-        [0., 0., 1.], 
-        [1., 0., 0.,]
+        [0., 0., 1.,], 
+        [1., 0., 0.], 
+        [0., 1., 0.,]
     ]) @ R_wc
     t = -R_wc @ pos
     ext_wc = np.hstack((R_wc, t))